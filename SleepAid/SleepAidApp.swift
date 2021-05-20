06c5f055ca7ed279ce75b738ffa20e34b9b96e87
hautera <hautea@uw.edu>
2021-05-19 23:53:43 -0700

    Initial Commit

---
+++ SleepAid/SleepAidApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepAidApp.swift
+//  SleepAid
+//
+//  Created by Austin Hauter on 5/19/21.
+//
+
+import SwiftUI
+
+@main
+struct SleepAidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
